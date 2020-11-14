6cbb5fdef434e1e8f9d8dceb9deb4d48804f1306
rowanG077 <goemansrowan@gmail.com>
2020-11-14 11:59:58 +0100

    Finished counter example

--- c-samples/counter.c
+++ c-samples/counter.c
@@ -3,7 +3,9 @@
 #include <inttypes.h>
 #include <pthread.h>
 
-void* counter(void* data_ptr) {
+static const size_t THREADCOUNT = 20;
+
+void* count(void* data_ptr) {
 	uint64_t* counter = (uint64_t*)data_ptr;
 
 	for (uint64_t i = 0; i < 1000000; ++i) {
@@ -14,10 +16,21 @@ void* counter(void* data_ptr) {
 }
 
 int main() {
-	pthread_t threads[100];
+	pthread_t threads[THREADCOUNT];
 
 	uint64_t counter = 0;
 
-	printf("Final counter value: %"PRIu64"\n");
+	for (size_t i = 0; i < THREADCOUNT; ++i) {
+		if (pthread_create(&threads[i], NULL, count, &counter)) {
+			perror("Error creating thread.");
+			exit(-1);
+		}
+	}
+
+	for (size_t i = 0; i < THREADCOUNT; ++i) {
+		pthread_join(threads[i], NULL);
+	}
+
+	printf("Final counter value: %"PRIu64"\n", counter);
 	return 0;
 }
